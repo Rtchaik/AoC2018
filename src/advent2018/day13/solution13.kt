edee6e10f538242695de4c8fee24c56bb38c7a07
Rtchaik <r_tchaikivsky@mail.ru>
2018-12-13 11:54:33 +0200

    Day 13 minor corrections

--- src/advent2018/day13/solution13.kt
+++ src/advent2018/day13/solution13.kt
@@ -7,10 +7,16 @@ import kotlin.system.measureTimeMillis
 
 fun main(args: Array<String>) {
     val executionTime = measureTimeMillis {
-        val carts = input.mapIndexed { row, s ->
-            s.mapIndexedNotNull { column, c ->
-                val cart = findCarts(c)
-                if (cart == null) null else Triple(Coordinates(column, row), cart, 0)
+        val carts = input.mapIndexed { rowIdx, row ->
+            row.mapIndexedNotNull { columnIdx, char ->
+                val cart = when (char) {
+                    '<' -> 3
+                    '^' -> 2
+                    '>' -> 1
+                    'v' -> 0
+                    else -> null
+                }
+                if (cart == null) null else Triple(Coordinates(columnIdx, rowIdx), cart, 0)
             }
         }
             .filter { it.isNotEmpty() }.flatten().toMutableList()
@@ -18,32 +24,23 @@ fun main(args: Array<String>) {
         var part1 = Coordinates(0, 0)
         while (carts.size > 1) {
             val crashed = mutableSetOf<Int>()
-            (0 until carts.size).forEach { current ->
-                carts[current] = makeMove(carts[current], input)
-                val node =
-                    carts.mapIndexedNotNull { idx, triple -> if (triple.first == carts[current].first) idx else null }
-                if (node.size > 1) {
-                    crashed.addAll(node)
-                    if (part1 == Coordinates(0, 0))  part1 = carts[node[0]].first
-
+            (0 until carts.size).forEach { currentIdx ->
+                carts[currentIdx] = makeMove(carts[currentIdx], input)
+                val nodeCars =
+                    carts.mapIndexedNotNull { idx, triple -> if (triple.first == carts[currentIdx].first) idx else null }
+                if (nodeCars.size > 1) {
+                    crashed.addAll(nodeCars)
+                    if (part1 == Coordinates(0, 0)) part1 = carts[nodeCars.first()].first
                 }
             }
             crashed.sortedDescending().forEach { carts.removeAt(it) }
         }
 
-        val part2 = carts[0].first
+        val part2 = carts.first().first
 
         println("Part 1: ${part1.column},${part1.row}\nPart 2: ${part2.column},${part2.row}")
     }
     println("Execution Time = $executionTime ms")
-}
-
-private fun findCarts(char: Char) = when (char) {
-    'v' -> 0
-    '<' -> 3
-    '^' -> 2
-    '>' -> 1
-    else -> null
 }
 
 private fun makeMove(cart: Triple<Coordinates, Int, Int>, grid: List<String>) =
@@ -54,21 +51,21 @@ private fun makeMove(cart: Triple<Coordinates, Int, Int>, grid: List<String>) =
             Triple(cart.first + directions[newDir], newDir, (cart.third + 1) % 3)
         }
         '\\' -> {
-            var turn = ""
-            when (cart.second) {
-                1, 3 -> turn = "left"
-                0, 2 -> turn = "right"
-            }
-            val newDir = turnInDirection(cart.second, turn)
+            val newDir = turnInDirection(
+                cart.second, when (cart.second) {
+                    1, 3 -> "left"
+                    else -> "right"
+                }
+            )
             Triple(cart.first + directions[newDir], newDir, cart.third)
         }
         '/' -> {
-            var turn = ""
-            when (cart.second) {
-                0, 2 -> turn = "left"
-                1, 3 -> turn = "right"
-            }
-            val newDir = turnInDirection(cart.second, turn)
+            val newDir = turnInDirection(
+                cart.second, when (cart.second) {
+                    0, 2 -> "left"
+                    else -> "right"
+                }
+            )
             Triple(cart.first + directions[newDir], newDir, cart.third)
         }
         else -> Triple(cart.first + directions[turnInDirection(cart.second, "straight")], cart.second, cart.third)
